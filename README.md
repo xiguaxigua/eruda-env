922a8832cb03a5b3c535e5f5855397a756671fd8
Melon <guang.xing@liulishuo.com>
2019-03-18 13:10:20 +0800

    feat: add basic logic

---
+++ README.md
@@ -0,0 +1,20 @@
+# eruda-env
+
+Eruda plugin for xxx.
+
+## Demo
+
+Browse it on your phone: 
+[http://eruda.liriliri.io/](http://eruda.liriliri.io/)
+
+## Install
+
+```bash
+npm install eruda-env --save
+```
+
+```javascript
+eruda.add(erudaEnv);
+```
+
+Make sure Eruda is loaded before this plugin, otherwise won't work.
---
+++ src/index.js
@@ -0,0 +1,46 @@
+module.exports = function(eruda) {
+  let { evalCss } = eruda.util;
+
+  class EnvSwitch extends eruda.Tool {
+    constructor(props) {
+      super();
+      this.name = 'env';
+      this._style = evalCss(require('./style.scss'));
+    }
+    init($el, container) {
+      super.init($el, container);
+      $el.html(require('./template.hbs')());
+      eruda.setEnv = envs => {
+        const box = $el.find('.eruda-box');
+        envs.forEach(({ name, url }) => {
+          const btn = document.createElement('div');
+          btn.addEventListener('click', event => {
+            const { url } = event.target.dataset;
+            if (url) {
+              window.location.href = window.location.href.replace(
+                window.location.origin,
+                url,
+              );
+            }
+          });
+          btn.className = 'eruda-btn';
+          btn.dataset.url = url;
+          btn.innerHTML = name;
+          box.get(0).appendChild(btn);
+        });
+      };
+    }
+    show() {
+      super.show();
+    }
+    hide() {
+      super.hide();
+    }
+    destroy() {
+      super.destroy();
+      evalCss.remove(this._style);
+    }
+  }
+
+  return new EnvSwitch();
+};
